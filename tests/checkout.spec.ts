6924a0605b541eff5abdf7876dffaa44f6d831fe
Arkadiusz Molenda <arkadiusz.molenda@clickmeeting.com>
2024-05-13 17:05:19 +0200

    add descriptions for checkout tests

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -3,12 +3,26 @@ import {test} from '@pages/fixtures/testBase'
 test.describe('tests of buying products from cart, and finalize checkout', () => {
 
 test('Buy products - happy path', async ({ page }) => {
+
 });
 
-test('Buy products - unhappy path', async ({ page }) => {
+test('Buy products - unhappy path invalid zip code', async ({ page }) => {
+
+});
+
+test('Buy products - unhappy path - empty customer information fields', async ({ page }) => {
+    
 });
 
 test('Buy products - back to change client information', async ({ page }) => {
+    
+});
+
+test('Buy products - back to cart', async ({ page }) => {
+    
+});
+
+test('Checkout with empty cart', async ({ page }) => {
     
 });
 
