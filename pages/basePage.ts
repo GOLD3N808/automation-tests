e1b9b77c094ee42009140b2ed7178a3b5bee22e9
Arkadiusz Molenda <arkadiusz.molenda@clickmeeting.com>
2024-05-13 16:59:10 +0200

    add burger menu tests

--- pages/basePage.ts
+++ pages/basePage.ts
@@ -5,12 +5,18 @@ class BasePage {
     burgerMenuButton: Locator
     logoutLink: Locator
     cartIcon: Locator
+    allItemsLink: Locator
+    aboutLink: Locator
+    resetAppStateLink: Locator
 
 constructor(page: Page){
 this.page = page
 this.burgerMenuButton = page.locator('#react-burger-menu-btn')
 this.logoutLink = page.locator('#logout_sidebar_link')
 this.cartIcon = page.locator('#shopping_cart_container')
+this.allItemsLink = page.locator('#inventory_sidebar_link')
+this.aboutLink = page.locator('#about_sidebar_link')
+this.resetAppStateLink = page.locator('#reset_sidebar_link')
 }
 
 async logout(){   
@@ -20,6 +26,21 @@ async logout(){
 
 async goToCart(){
     await this.cartIcon.click()
+}
+
+async goToAllItemsLink(){
+    await this.burgerMenuButton.click()
+    await this.allItemsLink.click()
+}
+
+async goToAboutLink(){
+    await this.burgerMenuButton.click()
+    await this.aboutLink.click()
+}
+
+async resetAppState(){
+    await this.burgerMenuButton.click()
+    await this.resetAppStateLink.click()
 }
 
 }
--- tests/userMenu.spec.ts
+++ tests/userMenu.spec.ts
@@ -1,18 +1,39 @@
 import {test} from '@pages/fixtures/testBase'
+import { user } from '@helpers/loginCredentials'
+import { expect } from '@playwright/test'
 
 test.describe('tests of links redirection from user menu', () => {
 
-test('Go to All Items', async ({ cartPage, dashboardPage }) => {
+test.beforeEach(async ({ page, loginPage, dashboardPage }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await loginPage.login(user['correctUsername'], process.env.CORRECT_PASSWORD)
     
+})
 
+test('Go to All Items', async ({ cartPage, page, dashboardPage }) => {
+    await dashboardPage.goToCart()
+    await cartPage.goToAllItemsLink()
+    expect(page.url()).toBe('https://www.saucedemo.com/inventory.html')
+    await expect(dashboardPage.dashboardContainer).toBeVisible()
 });
 
-test('Go to About', async ({ loginPage }) => {
-
+test('Go to About', async ({ dashboardPage, cartPage, page }) => {
+    await dashboardPage.goToCart()
+    await cartPage.goToAboutLink()
+    expect(page.url()).toBe('https://saucelabs.com/')
+    await expect(cartPage.cartContainer).toBeHidden()
 });
 
-test('Go to Reset App State', async ({ loginPage }) => {
-
+test('Go to Reset App State', async ({ dashboardPage, cartPage, loginPage }) => {
+    await dashboardPage.addProduct(0)
+    await dashboardPage.addProduct(1)
+    await dashboardPage.goToCart()
+    await cartPage.resetAppState()
+    await expect.soft(cartPage.productElement).toBeHidden()
+    await cartPage.backToDashboard()
+    await expect(dashboardPage.dashboardContainer).toBeVisible()
+    await expect(dashboardPage.removeButton(0)).toBeHidden()
+    await expect(dashboardPage.removeButton(1)).toBeHidden()
 });
 
 })
